b38519df42d99b55586badb14a1330474e7489fb
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-01-25 17:26:17 +0300

    init

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -5,11 +5,63 @@ import Sections from "./Sections/Sections";
 import TimeLine from "./Grid/TimeLine/TimeLine";
 
 export default function Body() {
+
+    const [scrollPositionY, setScrollPositionY] = React.useState(0);
+    const [scrollPositionX, setScrollPositionX] = React.useState(0);
+
+    const sectionsContainerRef = React.useRef<HTMLDivElement>(null);
+    const gridContainerRef = React.useRef<HTMLDivElement>(null);
+    const timeLineContainerRef = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        if (sectionsContainerRef.current && gridContainerRef.current) {
+            sectionsContainerRef.current.addEventListener('scroll', () => {
+                if (sectionsContainerRef.current) {
+                    setScrollPositionY(sectionsContainerRef.current.scrollTop)
+                }
+            });
+            gridContainerRef.current.addEventListener('scroll', () => {
+                if (gridContainerRef.current) {
+                    setScrollPositionY(gridContainerRef.current.scrollTop);
+                }
+            });
+        };
+
+        if (timeLineContainerRef.current && gridContainerRef.current) {
+            timeLineContainerRef.current.addEventListener('scroll', () => {
+                if (timeLineContainerRef.current) {
+                    setScrollPositionX(timeLineContainerRef.current.scrollLeft);
+                }
+            });
+            gridContainerRef.current.addEventListener('scroll', () => {
+                if (gridContainerRef.current) {
+                    setScrollPositionX(gridContainerRef.current.scrollLeft);
+                }
+            });
+        }
+    }, []);
+
+
+    React.useEffect(() => {
+        if (sectionsContainerRef.current && gridContainerRef.current) {
+            sectionsContainerRef.current.scrollTop = scrollPositionY;
+            gridContainerRef.current.scrollTop = scrollPositionY;
+        }
+
+        if (timeLineContainerRef.current && gridContainerRef.current) {
+            timeLineContainerRef.current.scrollLeft = scrollPositionX;
+            gridContainerRef.current.scrollLeft = scrollPositionX;
+        }
+        console.log("scrollPositionY");
+        console.log(scrollPositionY);
+    }, [scrollPositionX, scrollPositionY])
     return <>
-        <div className={styles.sections}><Sections/></div>
+        <div className={styles.sections} onScroll={() => {
+            setScrollPositionY(0)
+        }} ref={sectionsContainerRef}><Sections/></div>
         <div className={styles.body}>
-            <div className={styles.timeLine}><TimeLine/></div>
-            <div className={styles.grid} ><Grid/></div>
+            <div className={styles.timeLine} ref={timeLineContainerRef}><TimeLine/></div>
+            <div className={styles.grid} ref={gridContainerRef}><Grid/></div>
         </div>
     </>
 };
